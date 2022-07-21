7326dcab9ac952d7f54152fcfc4fe0ce64ce7126
GilbertAzevedo <gilbertif1995@outlook.com>
2022-07-21 14:27:07 +0000

    Exemplo OO - Versão 02

---
+++ Aula_0721/Categoria.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Categoria : IComparable<Categoria> {
+  public int Id { get; set; }
+  public string Descricao { get; set; }
+  public int CompareTo(Categoria obj) {
+    return Descricao.CompareTo(obj.Descricao);
+  }
+  public override string ToString() {
+    return $"{Id} - {Descricao}";
+  }
+}
